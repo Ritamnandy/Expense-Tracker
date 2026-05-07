6ebdeac66a1ded74bcbc070fc0ea412ad5d041ec
Ritamnandy <nandyritam519@gmail.com>
2026-05-07 20:10:54 +0530

    feat: update income page UI with form validation and input fields; add fl_chart dependency

--- client/lib/pages/income_page.dart
+++ client/lib/pages/income_page.dart
@@ -6,33 +6,72 @@ class Incomepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final formKey = GlobalKey<FormState>();
-    // final TextEditingController amountController = TextEditingController();
-    // final TextEditingController descriptionController = TextEditingController();
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(20),
-            height: 200,
-            width: double.infinity,
-            color: Colors.red,
-            child: Form(
-              key: formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
+    final TextEditingController amountController = TextEditingController();
+    final TextEditingController descriptionController = TextEditingController();
+    return GestureDetector(
+      onTap: () {
+        // FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          physics: CarouselScrollPhysics(),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(20),
+                height: 280,
+                width: double.infinity,
+                // color: Colors.red,
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: amountController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter amount';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          label: Text('Enter Amount'),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        controller: descriptionController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter amount';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          label: Text('Enter Purpose'),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            amountController.clear();
+                            descriptionController.clear();
+                          }
+                        },
+                        child: Text(
+                          "Submit",
+                          style: Theme.of(context).textTheme.bodyLarge,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
