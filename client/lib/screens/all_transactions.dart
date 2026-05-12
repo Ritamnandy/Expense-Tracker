28a094b6bd487c60a30ee487ec3494f52684fad7
Ritamnandy <nandyritam519@gmail.com>
2026-05-12 20:31:04 +0530

    feat: update AllTransaction screen with month picker and formatted date display; add Lottie and year_month_picker dependencies

--- client/lib/screens/all_transactions.dart
+++ client/lib/screens/all_transactions.dart
@@ -4,7 +4,9 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
+import 'package:year_month_picker/year_month_picker.dart';
 
 class AllTransaction extends StatefulWidget {
   final String currencySymbol;
@@ -16,12 +18,32 @@ class AllTransaction extends StatefulWidget {
 
 class _AllTransactionState extends State<AllTransaction> {
   String currentMonth = DateFormat('yyyy-MM').format(DateTime.now());
+  String formattedDate = DateFormat('MMMM yyyy').format(DateTime.now());
+
   @override
   void initState() {
     Future.microtask(() {
       context.read<ExpenseAndIncomeChart>().searchByMonth(currentMonth);
     });
     super.initState();
+  }
+
+  Future<void> pickMonth(BuildContext context) async {
+    final selected = await showYearMonthPickerDialog(
+      context: context,
+      firstYear: 2020,
+      lastYear: 2070,
+      initialYearMonth: DateTime.now(),
+    );
+    late String month;
+    if (selected != null) {
+      month = DateFormat('yyyy-MM').format(selected);
+      setState(() {
+        currentMonth = month;
+        formattedDate = DateFormat('MMMM yyyy').format(selected);
+      });
+    }
+    context.read<ExpenseAndIncomeChart>().searchByMonth(month);
   }
 
   @override
@@ -45,56 +67,87 @@ class _AllTransactionState extends State<AllTransaction> {
             Column(
               children: [
                 const SizedBox(height: 20),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: chartProvider.list.length,
-                  itemBuilder: (context, index) {
-                    final sortedList = [...chartProvider.list];
 
-                    sortedList.sort((a, b) => b.id!.compareTo(a.id!));
-                    DateTime date = DateTime.parse(sortedList[index].date);
-                    String formattedDate = DateFormat(
-                      'MMM dd, yyyy',
-                    ).format(date);
-                    if (sortedList.isEmpty) {
-                      return Center(child: Text("Data not entry"));
-                    }
-                    return ListTile(
-                      subtitle: Text(formattedDate),
-                      leading: Container(
-                        height: 35,
-                        width: 35,
-                        decoration: BoxDecoration(
-                          color: sortedList[index].isExpense
-                              ? Theme.of(context).colorScheme.secondary
-                              : Theme.of(context).colorScheme.primary,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Icon(
-                          sortedList[index].isExpense
-                              ? Icons.arrow_downward
-                              : Icons.arrow_upward,
-                          fontWeight: FontWeight.bold,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        formattedDate,
+                        style: Theme.of(
+                          context,
+                        ).textTheme.headlineSmall?.copyWith(fontSize: 21.sp),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          pickMonth(context);
+                        },
+                        icon: FaIcon(
+                          FontAwesomeIcons.filter,
+                          color: Theme.of(context).colorScheme.primary,
                         ),
                       ),
-                      title: Text(
-                        sortedList[index].purpose,
-                        style: TextStyle(
-                          fontSize: 17.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      trailing: Text(
-                        '${widget.currencySymbol} ${sortedList[index].amount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontSize: 17.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    );
-                  },
+                    ],
+                  ),
                 ),
+                const SizedBox(height: 5),
+
+                chartProvider.list.isEmpty
+                    ? Center(child: Lottie.asset("assets/images/notfound.json"))
+                    : ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: chartProvider.list.length,
+                        itemBuilder: (context, index) {
+                          final sortedList = [...chartProvider.list];
+
+                          sortedList.sort((a, b) => b.id!.compareTo(a.id!));
+                          DateTime date = DateTime.parse(
+                            sortedList[index].date,
+                          );
+                          String formattedDate = DateFormat(
+                            'MMM dd, yyyy',
+                          ).format(date);
+
+                          return ListTile(
+                            subtitle: Text(formattedDate),
+                            leading: Container(
+                              height: 35,
+                              width: 35,
+                              decoration: BoxDecoration(
+                                color: sortedList[index].isExpense
+                                    ? Theme.of(context).colorScheme.secondary
+                                    : Theme.of(context).colorScheme.primary,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Icon(
+                                sortedList[index].isExpense
+                                    ? Icons.arrow_downward
+                                    : Icons.arrow_upward,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            title: Text(
+                              sortedList[index].purpose,
+                              style: TextStyle(
+                                fontSize: 17.sp,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            trailing: Text(
+                              '${widget.currencySymbol} ${sortedList[index].amount.toStringAsFixed(2)}',
+                              style: TextStyle(
+                                fontSize: 17.sp,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          );
+                        },
+                      ).animate().fadeIn(
+                        curve: Curves.easeIn,
+                        duration: const Duration(milliseconds: 1000),
+                      ),
               ],
             ).animate().fadeIn(
               curve: Curves.easeIn,
